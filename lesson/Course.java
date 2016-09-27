6230503d7bf962a3ba86b664187b971e9ddc72ae
nvk3d <nike_bondar@mail.ru>
2016-09-27 13:46:04 +0300

    Head file of HomeWork

---
+++ lesson/Course.java
@@ -0,0 +1,18 @@
+package lesson;
+
+public class Course {
+    protected String[] mas = {"Run", "Jump", "Swim"};
+    
+    void doIt(Team team) {
+        int k = 0;
+        for(int i = 0; i < team.animal.length; i++) {
+            team.animal[i].isChallenge(mas[k]);
+            k++;
+            team.animal[i].isChallenge(mas[k]);
+            k++;
+            team.animal[i].isChallenge(mas[k]);
+            k = 0;
+        }
+    }
+    
+}
