d6728ebea9a93cd63a000eaffc4e13d9c0556757
nvk3d <nike_bondar@mail.ru>
2016-10-09 22:04:09 +0300

    edit server

--- Server/src/com/bondar/App.java
+++ Server/src/com/bondar/App.java
@@ -12,18 +12,5 @@ public class App {
         serverConnection.setPort(8189);
         serverConnection.init();
         ClientWindow clientWindow = new ClientWindow();
-        /*System.out.println("Выбери свою сторону, парень: server or client");
-        Scanner sc = new Scanner(System.in);
-        while (res.equals("OK.")) {
-            if (sc.nextLine().equalsIgnoreCase("server")) {
-                res = "OK.";
-                System.out.println(res);
-            } else if (sc.nextLine().equalsIgnoreCase("client")) {
-                res = "OK.";
-                System.out.println(res);
-            } else {
-                System.out.println("Нужно выбрать сторону: server or client");
-            }
-        }*/
     }
 }
